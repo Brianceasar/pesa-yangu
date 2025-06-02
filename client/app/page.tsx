98d416845b8dbb8c64ca6241fb638a6dfb4ae88a
brianceasar <brianceasar7@gmail.com>
2025-06-02 05:15:52 +0300

    misc commit -m

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -10,7 +10,7 @@ const LandingPage = () => {
       <Box sx={{ bgcolor: "primary.main", color: "white", py: 12 }}>
         <Container maxWidth="lg">
           <Typography variant="h2" fontWeight={700}>Your Journey to Financial Freedom</Typography>
-          <Typography variant="h6" mt={2}>Mentorship and Resources to Help You Thrive</Typography>
+          <Typography variant="h6" sx={{ mt: 2 }}>Mentorship and Resources to Help You Thrive</Typography>
           <Button variant="contained" color="secondary" size="large" sx={{ mt: 4 }} onClick={() => router.push("/register")}>
             Get Started
           </Button>
@@ -21,11 +21,11 @@ const LandingPage = () => {
         <Grid container spacing={4}>
           <Grid item xs={12} md={6}>
             <Typography variant="h4" fontWeight={600}>Learn from Trusted Mentors</Typography>
-            <Typography mt={2}>Book sessions with experienced professionals and mentors ready to guide you.</Typography>
+            <Typography sx={{ mt: 2 }}>Book sessions with experienced professionals and mentors ready to guide you.</Typography>
           </Grid>
           <Grid item xs={12} md={6}>
             <Typography variant="h4" fontWeight={600}>Access Valuable Resources</Typography>
-            <Typography mt={2}>Browse our curated library of financial education materials.</Typography>
+            <Typography sx={{ mt: 2 }}>Browse our curated library of financial education materials.</Typography>
           </Grid>
         </Grid>
       </Container>

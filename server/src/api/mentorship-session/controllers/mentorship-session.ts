aa5d40df0559a75a8833ab1cb2bf3e9d10e135df
gilbertmunuotz <officialgilbert24@gmail.com>
2025-06-03 14:25:26 +0300

    feat(api): finalize backend API with custom filters & RBAC

--- server/src/api/mentorship-session/controllers/mentorship-session.ts
+++ server/src/api/mentorship-session/controllers/mentorship-session.ts
@@ -2,6 +2,34 @@
  * mentorship-session controller
  */
 
-import { factories } from '@strapi/strapi'
+// src/api/mentorship-session/controllers/mentorship-session.ts
 
-export default factories.createCoreController('api::mentorship-session.mentorship-session');
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::mentorship-session.mentorship-session', ({ strapi }) => ({
+    async find(ctx) {
+        const user = ctx.state.user;
+
+        if (!user) {
+            return ctx.unauthorized("You must be logged in");
+        }
+
+        const roleType = user.role_type;
+
+        const filters: any = {};
+
+        if (roleType === 'customer') {
+            filters.customer = user.id;
+        } else if (roleType === 'mentor') {
+            filters.mentor = user.id;
+        }
+
+        ctx.query = {
+            ...ctx.query,
+            filters,
+        };
+
+        const { data, meta } = await super.find(ctx);
+        return { data, meta };
+    },
+}));
